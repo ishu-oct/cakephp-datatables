a2528efc97183f6304446ae147728465e90f1ebc
Ajith Kumar <ajith.kumar@zoomrx.com>
2019-08-06 16:22:45 +0530

    Added changes from zoomrx

--- src/Controller/Component/DataTablesComponent.php
+++ src/Controller/Component/DataTablesComponent.php
@@ -9,69 +9,75 @@ use Cake\ORM\TableRegistry;
  */
 class DataTablesComponent extends Component
 {
-
     protected $_defaultConfig = [
         'start' => 0,
         'length' => 10,
         'order' => [],
-        'conditionsOr' => [],  // table-wide search conditions
+        'conditionsOr' => [], // table-wide search conditions
         'conditionsAnd' => [], // column search conditions
-        'matching' => [],      // column search conditions for foreign tables
+        'matching' => [], // column search conditions for foreign tables
     ];
 
     protected $_viewVars = [
         'recordsTotal' => 0,
         'recordsFiltered' => 0,
-        'draw' => 0
+        'draw' => 0,
     ];
 
     protected $_isAjaxRequest = false;
+
+    protected $_applyLimit = true;
 
     protected $_tableName = null;
 
     protected $_plugin = null;
 
+    //TODO: Rewrite it
+    private $queryFields = []; //To handle computed fields in conditions
+
     /**
      * Process query data of ajax request
      *
      */
-    private function _processRequest()
+    private function _processRequest($param = null)
     {
         // -- check whether it is an ajax call from data tables server-side plugin or a normal request
         $this->_isAjaxRequest = $this->request->is('ajax');
 
-        // -- add limit
-        if( isset($this->request->query['length']) && !empty($this->request->query['length']) )
-        {
-            $this->config('length', $this->request->query['length']);
+        // -- check whether it is an csv request
+        if (isset($this->request->query['_csv_output']) && $this->request->query['_csv_output'] == true) {
+            $this->_applyLimit = false;
         }
 
-        // -- add offset
-        if( isset($this->request->query['start']) && !empty($this->request->query['start']) )
-        {
-            $this->config('start', (int)$this->request->query['start']);
+        if ($this->_applyLimit === true) {
+            // -- add limit
+            if (isset($this->request->query['length']) && !empty($this->request->query['length'])) {
+                $this->config('length', $this->request->query['length']);
+            }
+
+            // -- add offset
+            if (isset($this->request->query['start']) && !empty($this->request->query['start'])) {
+                $this->config('start', (int)$this->request->query['start']);
+            }
         }
 
         // -- add order
-        if( isset($this->request->query['order']) && !empty($this->request->query['order']) )
-        {
+        if (isset($this->request->query['order']) && !empty($this->request->query['order'])) {
             $order = $this->config('order');
-            foreach($this->request->query['order'] as $item) {
+            foreach ($this->request->query['order'] as $item) {
                 $order[$this->request->query['columns'][$item['column']]['name']] = $item['dir'];
             }
             $this->config('order', $order);
         }
 
         // -- add draw (an additional field of data tables plugin)
-        if( isset($this->request->query['draw']) && !empty($this->request->query['draw']) )
-        {
+        if (isset($this->request->query['draw']) && !empty($this->request->query['draw'])) {
             $this->_viewVars['draw'] = (int)$this->request->query['draw'];
         }
 
         // -- don't support any search if columns data missing
-        if( !isset($this->request->query['columns']) ||
-            empty($this->request->query['columns']) )
-        {
+        if (!isset($this->request->query['columns']) ||
+            empty($this->request->query['columns'])) {
             return;
         }
 
@@ -80,66 +86,193 @@ class DataTablesComponent extends Component
             $this->request->query['search']['value'] : false);
 
         // -- add conditions for both table-wide and column search fields
-        foreach($this->request->query['columns'] as $column)
-        {
-            if( $globalSearch && $column['searchable'] == 'true' ) {
-                $this->_addCondition( $column['name'], $globalSearch, 'or' );
-            }
-            $localSearch = $column['search']['value'];
-            /* In some circumstances (no "table-search" row present), DataTables
-               fills in all column searches with the global search. Compromise:
-               Ignore local field if it matches global search. */
-            if( !empty($localSearch) && ($localSearch !== $globalSearch) ) {
-                $this->_addCondition( $column['name'], $column['search']['value'] );
+        foreach ($this->request->query['columns'] as $column) {
+            if (!empty($column['name'])) {
+                if ($globalSearch && $column['searchable'] == 'true') {
+                    $this->_addCondition($column['name'], $globalSearch, 'or', true);
+                }
+                $localSearch = $column['search']['value'];
+                /* In some circumstances (no "table-search" row present), DataTables
+                fills in all column searches with the global search. Compromise:
+                Ignore local field if it matches global search. */
+                if ((isset($localSearch) && strlen($localSearch) > 0) && ($localSearch !== $globalSearch)) {
+                    if (isset($column['search']['regex']) && $column['search']['regex'] != 'false') {
+                        /*  Hack regex field is used for wild card search or full search options.
+                            If regex field is true then we have to use full search (column must equal to value)
+                            otherwise we can perform wild card search (%LIKE%) */
+                        $this->_addCondition($column['name'], $column['search']['value'], 'and', false);
+                    } else {
+                        $this->_addCondition($column['name'], $column['search']['value'], 'and', true);
+                    }
+                }
             }
         }
+    }
 
+    /**
+     * Get data paths for CSV export
+     *
+     * @return array
+     */
+    public function getPaths()
+    {
+        $parser = [];
+        foreach ($this->request->query['columns'] as $column) {
+            if (!empty($column['name']) && !empty($column['data'])) {
+                $parser[] = $column['data'];
+            }
+        }
+        return $parser;
+    }
+
+    /**
+     * Get data header for CSV export
+     *
+     * @return array
+     */
+    public function getHeader()
+    {
+        $header = [];
+        foreach ($this->request->query['columns'] as $column) {
+            if (!empty($column['name'])) {
+                $column = str_replace('_matchingData.', '', $column['name']);
+                $header[] = $column;
+            }
+        }
+        return $header;
+    }
+
+    /**
+     * Check if specific array key exists in multidimensional array
+     * @param array $arr - An array with keys to check.
+     * @param string $lookup - Value to check.
+     * @return Returns path of the key on success or null on failure.
+     */
+    public function findPath($arr, $lookup)
+    {
+        if (array_key_exists($lookup, $arr)) {
+            return [$lookup];
+        } else {
+            foreach ($arr as $key => $subarr) {
+                if (is_array($subarr)) {
+                    $ret = $this->findPath($subarr, $lookup);
+
+                    if ($ret) {
+                        $ret[] = $key;
+                        return $ret;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    /**
+     * Check if specific array key exists in multidimensional array
+     * @param array $arr - An array with keys to check.
+     * @param string $lookup - Value to check.
+     * @return Returns dot notation path of the key on success or FALSE on failure.
+     */
+    public function getKeyPath($arr, $lookup)
+    {
+        $path = $this->findPath($arr, $lookup);
+        if ($path === null) {
+            return false;
+        } else {
+            return implode('.', array_reverse($path));
+        }
     }
 
     /**
      * Find data
      *
-     * @param $tableName
-     * @param $finder
-     * @param array $options
+     * @param string $tableName Name of the table
+     * @param string $finder all,list.,
+     * @param array $options query options
      * @return array|\Cake\ORM\Query
      */
     public function find($tableName, $finder = 'all', array $options = [])
     {
-
         // -- get table object
         $table = TableRegistry::get($tableName);
-        $this->_tableName = $table->alias();
+        $data = $table->find($finder, $options);
+        return $this->process($data);
+    }
+
+    /**
+     * Process queryObject
+     *
+     * @param \Cake\ORM\Query  $queryObject Query object to be processed
+     * @param array $filterParams used for post request instead of ajax
+     * @param bool $applyLimit boolean to set Limit
+     * @return array|\Cake\ORM\Query
+     */
+    public function process($queryObject, $filterParams = null, $applyLimit = null)
+    {
+        $this->_setQueryFields($queryObject);
+
+        //set table alias name
+        $this->_tableName = $queryObject->repository()->alias();
+
+        if ($filterParams !== null) {
+            $this->request->query['columns'] = $filterParams['columns'];
+            $this->request->query['search'] = $filterParams['search'];
+            $this->_applyLimit = false;
+        }
+
+        if ($applyLimit !== null) {
+            $this->_applyLimit = $applyLimit;
+        }
 
         // -- get query options
         $this->_processRequest();
-        $data = $table->find($finder, $options);
 
         // -- record count
-        $this->_viewVars['recordsTotal'] = $data->count();
+        $this->_viewVars['recordsTotal'] = $queryObject->count();
 
         // -- filter result
-        $data->where($this->config('conditionsAnd'));
-        foreach($this->config('matching') as $association => $where) {
-            $data->matching( $association, function ($q) use ($where) {
-                return $q->where($where);
-            });
-        };
-        $data->andWhere(['or' => $this->config('conditionsOr')]);
+        if (count($this->config('conditionsAnd')) > 0) {
+            $queryObject->where($this->config('conditionsAnd'));
+        }
 
-        $this->_viewVars['recordsFiltered'] = $data->count();
+        foreach ($this->config('matching') as $association => $where) {
+            /*$associationPath = $this->getKeyPath($queryObject->contain(), $association);
+            if ($associationPath !== false) {
+                $queryObject->contain([
+                    $associationPath => function ($q) use ($where) {
+                        return $q->where($where);
+                    },
+                ]);
+            } elseif (isset($queryObject->join()[$association])) {
+                $queryObject->andWhere($where);
+            } else {
+                $queryObject->matching($association, function ($q) use ($where) {
+                    return $q->where($where);
+                });
+            }*/
+            $queryObject->where($where);
+        }
 
-        // -- add limit
-        $data->limit( $this->config('length') );
-        $data->offset( $this->config('start') );
+        if (count($this->config('conditionsOr')) > 0) {
+            $queryObject->andWhere(['or' => $this->config('conditionsOr')]);
+        }
+
+        //->bufferResults(true) Hack => when we add inner join cond the count will be returned from cache not actual count
+        $this->_viewVars['recordsFiltered'] = $queryObject->bufferResults(true)->count();
+
+        if ($this->_applyLimit === true) {
+            // -- add limit
+            $queryObject->limit($this->config('length'));
+            $queryObject->offset($this->config('start'));
+        }
 
         // -- sort
-        $data->order( $this->config('order') );
+        $queryObject->order($this->config('order'));
 
         // -- set all view vars to view and serialize array
         $this->_setViewVars();
-        return $data;
 
+        return $queryObject;
     }
 
     private function _getController()
@@ -149,28 +282,68 @@ class DataTablesComponent extends Component
 
     private function _setViewVars()
     {
-        $_serialize = [];
-        foreach($this->_viewVars as $field => $value) {
-            $_serialize[] = $field;
-        }
         $this->_getController()->set($this->_viewVars);
-        $this->_getController()->set('_serialize', $_serialize);
+        $this->_getController()->set('_serialize', array_keys($this->_viewVars));
+        if (isset($this->request->query['_csv_output']) && $this->request->query['_csv_output'] == true) {
+            // In case of CSV download, set csv headers and the fields to be inserted into csv file
+            $this->_getController()->set('_header', $this->getHeader());
+            $this->_getController()->set('_extract', $this->getPaths());
+        }
     }
 
-    private function _addCondition($column, $value, $type = 'and')
+    private function _addCondition($column, $value, $type = 'and', $isWildcardSearch = true)
     {
-        $condition = ["$column LIKE" => "$value%"];
-
-        if( $type === 'or' ) {
+        $columnObj = $this->_getColumnName($column);
+        $condition = [];
+        if ($isWildcardSearch == true) {
+            $condition = [$this->queryObject->newExpr()->like($columnObj, "%$value%", 'string')];
+        } else {
+            if (is_null($value) || $value == 'null' || $value == 'NULL') {
+                $condition = [$column . ' IS NULL'];
+            } else {
+                $condition[$column] = $value;
+            }
+        }
+        
+        if ($type === 'or') {
             $this->config('conditionsOr', $condition); // merges
             return;
-        }
-
-        list($association, $field) = explode('.', $column);
-        if( $this->_tableName == $association) {
-            $this->config('conditionsAnd', $condition); // merges
         } else {
-            $this->config('matching', [$association => $condition]); // merges
+            $pieces = explode('.', $column);
+            if (count($pieces) > 1) {
+                list($association, $field) = $pieces;
+                if ($this->_tableName == $association) {
+                    $this->config('conditionsAnd', $condition); // merges
+                } else {
+                    $this->config('matching', [$association => $condition]); // merges
+                }
+            } else {
+                $this->config('conditionsAnd', $condition); // merges
+            }
         }
+    }
+
+    /**
+     * Replace all computed columns
+     * @param string $name
+     * @return string
+     */
+    private function _getColumnName($name)
+    {
+        if (isset($this->queryFields[$name])) {
+            return $this->queryFields[$name];
+        } else {
+            return $name;
+        }
+    }
+
+    /**
+     * Set the query object and select fields, so that it can be used in conditions
+     * @param Query $queryObject
+     */
+    private function _setQueryFields($queryObject)
+    {
+        $this->queryObject = $queryObject;
+        $this->queryFields = $queryObject->clause('select');
     }
 }
--- src/View/Helper/DataTablesHelper.php
+++ src/View/Helper/DataTablesHelper.php
@@ -1,10 +1,10 @@
 <?php
 namespace DataTables\View\Helper;
 
-use Cake\View\Helper;
-use Cake\View\View;
 use Cake\ORM\Query;
+use Cake\View\Helper;
 use Cake\View\StringTemplateTrait;
+use Cake\View\View;
 
 /**
  * DataTables helper
@@ -16,47 +16,27 @@ class DataTablesHelper extends Helper
 
     use StringTemplateTrait;
 
-    protected $_defaultConfig = [
-        'ajax' => [
-            'dataSrc' => 'data'
-        ],
-        'searching' => true,
-        'processing' => true,
-        'serverSide' => true,
-        'deferRender' => true,
-        'dom' => '<<"row"<"col-sm-4"i><"col-sm-8"lp>>rt>',
-        'delay' => 600
-    ];
+    protected $_defaultConfig = [];
 
     public function init(array $options = [])
     {
-        $this->_templater = $this->templater();
+        // if (isset($options['buttons']) && !isset($options['dom'])) {
+        //     $options['dom'] = "<'row'<'col-sm-6'l><'col-sm-6'f>><'row'<'col-sm-12'tr>><'row'<'col-sm-5'i><'col-sm-7'p>>B";
+        // }
         $this->config($options);
-
-        // -- load i18n
-        $this->config('language', [
-            'paginate' => [
-                'next' => '<i class="fa fa-chevron-right"></i>',
-                'previous' => '<i class="fa fa-chevron-left"></i>'
-            ],
-            'processing' => __d('DataTables', 'Your request is processing ...'),
-            'lengthMenu' =>
-                '<select class="form-control">' .
-                '<option value="10">' . __d('DataTables', 'Display {0} records', 10) . '</option>' .
-                '<option value="25">' . __d('DataTables', 'Display {0} records', 25) . '</option>' .
-                '<option value="50">' . __d('DataTables', 'Display {0} records', 50) . '</option>' .
-                '<option value="100">' .__d('DataTables', 'Display {0} records', 100) . '</option>' .
-                '</select>',
-            'info' => __d('DataTables', 'Showing _START_ to _END_ of _TOTAL_ entries'),
-            'infoFiltered' => __d('DataTables', '(filtered from _MAX_ total entries)')
-        ]);
 
         return $this;
     }
 
     public function draw($selector)
     {
-        return sprintf('delay=%d;table=jQuery("%s").dataTable(%s);initSearch();', $this->config('delay'), $selector, json_encode($this->config()) );
+        $json = json_encode($this->config());
+        //Reset config
+        $this->_config = [];
+        $json = preg_replace('/"callback:(.*?)"/', '$1', $json);
+        $js = "(function () { \n var cakeDataTableOptions = $json;\n";
+        $js .= "return initDataTable('$selector', cakeDataTableOptions);\n }).call(null)";
+        return $js;
     }
-
 }
+
